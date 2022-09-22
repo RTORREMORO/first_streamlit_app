8a4dbd37b553d8d778bf234dd5acb350dcab85e5
Ricardo Jr Torremoro <114126734+RTORREMORO@users.noreply.github.com>
2022-09-22 11:57:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.hearder('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+strealit.text(('Hard-Boiled Free-Range Egg')
