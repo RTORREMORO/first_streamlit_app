4f721e9988b14b42f3e10f53f8a01e0f59b6c1c1
Ricardo Jr Torremoro <114126734+RTORREMORO@users.noreply.github.com>
2022-10-04 18:21:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,20 +26,19 @@ fruit_to_show = my_fruit_list.loc[fruit_selected]
 # Display the table on the page.
 streamlit.dataframe(fruit_to_show)
 
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
-    return fruityvice_normalized
-
 streamlit.header('Fruityvice Fruit Advice')
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
-        
+         fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+         streamlit.dataframe(fruityvice_normalized)
+            
+except URLError as e:
+    streamlit.error()
+    
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get _fruit_load_list():
